9b9cd336d7f28828012b30e6dd047b385ae4dd72
ZairDinahet <dinahetzair@gmail.com>
2024-09-21 23:25:24 -0300

    feat(EmployeeRepository.java): list employee

--- src/main/java/com/apirest/api_rest_java/repositories/EmployeeRepository.java
+++ src/main/java/com/apirest/api_rest_java/repositories/EmployeeRepository.java
@@ -3,6 +3,11 @@ package com.apirest.api_rest_java.repositories;
 import com.apirest.api_rest_java.models.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByCompanyId(Long companyId);
+
 }
 
