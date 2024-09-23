95c2cb9434161aa932e73d437d4cbb4f84f046d1
ZairDinahet <dinahetzair@gmail.com>
2024-09-23 19:42:31 -0300

    feat(ApiRestJavaApplication.java): indentation style

--- src/main/java/com/apirest/api_rest_java/ApiRestJavaApplication.java
+++ src/main/java/com/apirest/api_rest_java/ApiRestJavaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiRestJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiRestJavaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestJavaApplication.class, args);
+    }
 }
