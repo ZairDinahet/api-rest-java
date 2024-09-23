a30c7aff6d36b4b2ce39953935044da358965ea3
ZairDinahet <dinahetzair@gmail.com>
2024-09-23 19:39:23 -0300

    feat(GlobalExceptionHandler.java): global exception handler created

---
+++ src/main/java/com/apirest/api_rest_java/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.apirest.api_rest_java.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getStatus().value());
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
