a0610ef0fbb4dcb15a80f3df8c4fc90dae749a23
ZairDinahet <dinahetzair@gmail.com>
2024-09-23 19:35:34 -0300

    feat(CompanyServiceImp.java): company service implementation created

---
+++ src/main/java/com/apirest/api_rest_java/services/CompanyServiceImp.java
@@ -0,0 +1,63 @@
+package com.apirest.api_rest_java.services;
+
+import com.apirest.api_rest_java.exceptions.CustomException;
+import com.apirest.api_rest_java.mappers.CompanyMapper;
+import com.apirest.api_rest_java.models.dtos.CompanyDTO;
+import com.apirest.api_rest_java.models.entities.Company;
+import com.apirest.api_rest_java.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CompanyServiceImp implements CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private CompanyMapper companyMapper;
+
+    @Override
+    public List<CompanyDTO> getAllCompanies() {
+        List<Company> companies = companyRepository.findAll();
+        return companies.stream().map(companyMapper::convertToDTO) //.map(company -> companyMapper.convertToDTO(company))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CompanyDTO getCompanyById(Long id) {
+        Company company = companyRepository.findById(id).orElseThrow(() -> new CustomException("Company not found with id: " + id, HttpStatus.NOT_FOUND));
+        return companyMapper.convertToDTO(company);
+    }
+
+    @Override
+    public CompanyDTO saveCompany(CompanyDTO companyDTO) {
+        Company company = companyMapper.convertToEntity(companyDTO);
+        Company savedCompany = companyRepository.save(company);
+        return companyMapper.convertToDTO(savedCompany);
+    }
+
+    @Override
+    public CompanyDTO updateCompany(Long id, CompanyDTO companyDTO) {
+        Company existingCompany = companyRepository.findById(id)
+                .orElseThrow(() -> new CustomException("Company not found with id: " + id, HttpStatus.NOT_FOUND));
+
+        existingCompany.setName(companyDTO.getName());
+        existingCompany.setAddress(companyDTO.getAddress());
+
+        Company updatedCompany = companyRepository.save(existingCompany);
+
+        return companyMapper.convertToDTO(updatedCompany);
+    }
+
+    @Override
+    public void deleteCompanyById(Long id) {
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() -> new CustomException("Company not found with id: " + id, HttpStatus.NOT_FOUND));
+        companyRepository.delete(company);
+    }
+}
