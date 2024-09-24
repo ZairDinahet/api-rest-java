d4b5ce3fe4890f60469dec31de89b71ad19826cc
ZairDinahet <dinahetzair@gmail.com>
2024-09-24 01:29:14 -0300

    fix(EmployeeServiceimp.java): injection by constructor and fix in update employee

--- src/main/java/com/apirest/api_rest_java/services/EmployeeServiceImp.java
+++ src/main/java/com/apirest/api_rest_java/services/EmployeeServiceImp.java
@@ -7,29 +7,26 @@ import com.apirest.api_rest_java.models.entities.Company;
 import com.apirest.api_rest_java.models.entities.Employee;
 import com.apirest.api_rest_java.repositories.CompanyRepository;
 import com.apirest.api_rest_java.repositories.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Service
 public class EmployeeServiceImp implements EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    private EmployeeMapper employeeMapper;
-
-    @Autowired
-    private CompanyRepository companyRepository;
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+    private final CompanyRepository companyRepository;
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
         List<Employee> employees = employeeRepository.findAll();
-        return employees.stream().map(employeeMapper::convertToDTO)
+        return employees.stream()
+                .map(employeeMapper::convertToDTO)
                 .collect(Collectors.toList());
     }
 
@@ -66,12 +63,16 @@ public class EmployeeServiceImp implements EmployeeService {
         Employee existingEmployee = employeeRepository.findById(id)
                 .orElseThrow(() -> new CustomException("Employee not found with id: " + id, HttpStatus.NOT_FOUND));
 
+        Company company = companyRepository.findById(employeeDTO.getCompanyId())
+                .orElseThrow(() -> new CustomException("Company not found with id: " + employeeDTO.getCompanyId(), HttpStatus.NOT_FOUND));
+
         existingEmployee.setName(employeeDTO.getName());
         existingEmployee.setLastName(employeeDTO.getLastName());
         existingEmployee.setAddress(employeeDTO.getAddress());
         existingEmployee.setEmail(employeeDTO.getEmail());
         existingEmployee.setPhone(employeeDTO.getPhone());
         existingEmployee.setPosition(employeeDTO.getPosition());
+        existingEmployee.setCompany(company);
 
         Employee updatedEmployee = employeeRepository.save(existingEmployee);
 
