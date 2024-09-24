f2db5bb3e6711e0484b12a466e63e211514d1204
ZairDinahet <dinahetzair@gmail.com>
2024-09-24 01:27:17 -0300

    feat(EmployeeDTO.java): validations

--- src/main/java/com/apirest/api_rest_java/models/dtos/EmployeeDTO.java
+++ src/main/java/com/apirest/api_rest_java/models/dtos/EmployeeDTO.java
@@ -1,17 +1,30 @@
 package com.apirest.api_rest_java.models.dtos;
-
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EmployeeDTO {
-    private Long id;         // Identificador único del empleado
-    private String name;     // Nombre del empleado
-    private String lastName; // Apellido del empleado
-    private String position; // Cargo del empleado
-    private String address;  // Dirección del empleado
-    private String email;    // Correo electrónico del empleado
-    private String phone;    // Teléfono del empleado
-    private Long companyId;  // Identificador único de la empresa
+    private Long id;
+
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    @NotEmpty(message = "Last name is required")
+    private String lastName;
+
+    @NotEmpty(message = "Position is required")
+    private String position;
+
+    @NotEmpty(message = "Address is required")
+    private String address;
+
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    @NotEmpty(message = "Phone is required")
+    private String phone;
+
+    private Long companyId;
 }
